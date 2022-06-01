9aca0110876778e9971d8358dbeae53e3f5920e6
Gabriel  Melo <gabrieldejesusmelo0@gmail.com>
2022-06-01 20:32:19 -0300

    Add files via upload

---
+++ AulaLogin2/src/PassWord/pASSwORD.java
@@ -0,0 +1,29 @@
+package PassWord;
+
+public class pASSwORD {
+
+	private String Password;
+	private String User;
+	private String ConfPassword;
+
+
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	public String getUser() {
+		return User;
+	}
+	public void setUser(String user) {
+		User = user;
+	}
+	public String getConfPassword() {
+		return ConfPassword;
+	}
+	public void setConfPassword(String confPassword) {
+		ConfPassword = confPassword;
+	}
+
+}
